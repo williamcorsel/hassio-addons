2148cc2c90b347f332421bb0dde892f24356d516
williamcorsel <c.w.corsel@umail.leidenuniv.nl>
2023-02-21 12:08:00 +0100

    Add template scripts

---
+++ .templates/options_to_env.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+
+# ==============================================================================
+# Export all addon options as env 
+# Adapted from alexbelgium:
+# https://github.com/alexbelgium/hassio-addons/blob/master/.templates/00-global_var.sh
+# ==============================================================================
+
+# For all keys in options.json
+JSONSOURCE="/data/options.json"
+
+# Export keys as env variables
+# echo "All addon options were exported as variables"
+mapfile -t arr < <(jq -r 'keys[]' "${JSONSOURCE}")
+
+for KEYS in "${arr[@]}"; do
+    # export key
+    VALUE=$(jq ."$KEYS" "${JSONSOURCE}")
+    VALUE="${VALUE//[\"\']/}"
+    line="${KEYS}='${VALUE}'"
+    # Check if secret
+    if [[ "${line}" == *'!secret '* ]]; then
+        echo "secret detected"
+        secret=${line#*secret }
+        # Check if single match
+        secretnum=$(sed -n "/$secret:/=" /config/secrets.yaml)
+        [[ "$secretnum" == *' '* ]] && bashio::exit.nok "There are multiple matches for your password name. Please check your secrets.yaml file"
+        # Get text
+        secret=$(sed -n "/$secret:/p" /config/secrets.yaml)
+        secret=${secret#*: }
+        line="${line%%=*}='$secret'"
+        VALUE="$secret"
+    fi
+    # text
+    if bashio::config.false "verbose" || [[ "${KEYS}" == *"PASS"* ]]; then
+        bashio::log.blue "${KEYS}=******"
+    else
+        bashio::log.blue "$line"
+    fi
+
+    ######################################
+    # Export the variable to run scripts #
+    ######################################
+    export "${KEYS}='${VALUE}'"
+    echo "${KEYS}='${VALUE}'}"
+
+    # For non s6
+    if cat /etc/services.d/*/*run* &>/dev/null; then sed -i "1a export $line" /etc/services.d/*/*run* 2>/dev/null; fi
+    if cat /etc/cont-init.d/*run* &>/dev/null; then sed -i "1a export $line" /etc/cont-init.d/*run* 2>/dev/null; fi
+    # For s6
+    if [ -d /var/run/s6/container_environment ]; then printf "%s" "${VALUE}" > /var/run/s6/container_environment/"${KEYS}"; fi
+
+done
