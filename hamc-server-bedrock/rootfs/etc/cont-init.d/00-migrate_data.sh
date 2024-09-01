ecb09ccdfd703ddc8473a7bfe1f85da43d321207
Oliver Thorp <20244594+othorp@users.noreply.github.com>
2024-09-01 00:47:01 +0000

    Fix addon folder deletion

--- hamc-server-bedrock/rootfs/etc/cont-init.d/00-migrate_data.sh
+++ hamc-server-bedrock/rootfs/etc/cont-init.d/00-migrate_data.sh
@@ -5,6 +5,7 @@ if [ -d /addons/hamc-server-bedrock/data ]; then
   if [ -z "$(ls -A /config)" ]; then
     bashio::log.info "Migrating minecraft data from /addons to /addon_configs"
     mv /addons/hamc-server-bedrock/data/* /config
+    rmdir /addons/hamc-server-bedrock/data /addons/hamc-server-bedrock
   else
     bashio::log.warning "HAMC config files are present at both /addons/hamc-server-bedrock/data/ and /addon_configs/"
     bashio::log.warning "Manual migration of minecraft data to /addon_configs/ may be required"
